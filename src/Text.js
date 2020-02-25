5122fc77c6ea81247a497741f185d2f687b2b694
unknown <gaber32188@gmail.com>
2020-02-25 12:03:14 +0300

    added based datepicker functionality

--- src/Text.js
+++ src/Text.js
@@ -1,25 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import SaveButton from './SaveButton.js';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default function Text({ data, fullTr }) {
-  const editableClasses = [
-    'text',
-    'publishDate',
-    'publishHour',
-    'isPaid',
-    'isDeleted'
-  ];
+  const editableClasses = ['text', 'publishHour', 'isPaid', 'isDeleted'];
+  const dateClasses = ['publishDate'];
 
   const [displaySaveButton, setDisplaySaveButton] = useState(false);
   const [dimensions, setDimensions] = useState();
   const [currentTxt, setCurrentTxt] = useState(data[1]);
+  const [publishDate, setPublishDate] = useState();
+  const span = useRef();
 
   const hasEditableClass = editableClasses.includes(data[0]);
+  const isDate = dateClasses.includes(data[0]);
+
+  if (isDate && !publishDate) {
+    if (data[1]) {
+      const dateArr = data[1].split('-');
+      let year = Number(dateArr[0]);
+      let month = Number(dateArr[1]) - 1;
+      let day = Number(dateArr[2]);
+      setPublishDate(new Date(year, month, day));
+    }
+  }
 
   const showSaveButton = e => {
     let dimensions = e.target.parentNode.getBoundingClientRect();
     setDimensions(dimensions);
     setDisplaySaveButton(true);
+  };
+
+  const showDatePicker = e => {
+    console.log('showing picker');
   };
 
   const hideSaveButton = e => {
@@ -30,10 +45,12 @@ export default function Text({ data, fullTr }) {
   const saveTxt = e => {
     setCurrentTxt(e.target.previousSibling.textContent);
   };
+
   return (
     <>
       {hasEditableClass && (
         <span
+          ref={span}
           contentEditable={true}
           suppressContentEditableWarning={true}
           onFocus={e => {
@@ -46,12 +63,14 @@ export default function Text({ data, fullTr }) {
           onInput={e => {
             if (displaySaveButton) return;
             showSaveButton(e);
+            showDatePicker(e);
           }}
         >
           {currentTxt}
         </span>
       )}
-      {!hasEditableClass && <span>{data[1]}</span>}
+      {isDate && <DatePicker selected={publishDate} />}
+      {!hasEditableClass && !isDate && <span>{data[1]}</span>}
       <SaveButton
         displaySaveButton={displaySaveButton}
         dimensions={dimensions}
