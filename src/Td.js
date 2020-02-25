ab59d12169a373a4d4c624df3a0e71537f222d38
unknown <gaber32188@gmail.com>
2020-02-25 14:25:13 +0300

    fixed DatePicker

--- src/Td.js
+++ src/Td.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Text from './Text';
 import uuid from 'react-uuid';
 
 export default function Td(props) {
+  const tdRef = useRef();
   return (
-    <td key={uuid()} className={props.td[0]}>
-      <Text data={props.td} fullTr={props.fullTr} />
+    <td ref={tdRef} key={uuid()} className={props.td[0]}>
+      <Text data={props.td} fullTr={props.fullTr} tdRef={tdRef} />
     </td>
   );
 }
--- src/Text.js
+++ src/Text.js
@@ -1,10 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
 import SaveButton from './SaveButton.js';
 import DatePicker from 'react-datepicker';
+import Backend from './Backend.js';
 
 import 'react-datepicker/dist/react-datepicker.css';
 
-export default function Text({ data, fullTr }) {
+export default function Text({ data, fullTr, tdRef }) {
   const editableClasses = ['text', 'publishHour', 'isPaid', 'isDeleted'];
   const dateClasses = ['publishDate'];
 
@@ -18,23 +19,13 @@ export default function Text({ data, fullTr }) {
   const isDate = dateClasses.includes(data[0]);
 
   if (isDate && !publishDate) {
-    if (data[1]) {
-      const dateArr = data[1].split('-');
-      let year = Number(dateArr[0]);
-      let month = Number(dateArr[1]) - 1;
-      let day = Number(dateArr[2]);
-      setPublishDate(new Date(year, month, day));
-    }
+    if (data[1]) setPublishDate(new Date(data[1]));
   }
 
   const showSaveButton = e => {
-    let dimensions = e.target.parentNode.getBoundingClientRect();
+    let dimensions = tdRef.current.getBoundingClientRect();
     setDimensions(dimensions);
     setDisplaySaveButton(true);
-  };
-
-  const showDatePicker = e => {
-    console.log('showing picker');
   };
 
   const hideSaveButton = e => {
@@ -46,6 +37,26 @@ export default function Text({ data, fullTr }) {
     setCurrentTxt(e.target.previousSibling.textContent);
   };
 
+  const saveSelectedDate = date => {
+    setPublishDate(date);
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    if (month < 10) month = `0${month}`;
+    let day = date.getUTCDate();
+    if (day < 10) day = `0${day}`;
+
+    const sendObj = {
+      id: fullTr.id,
+      [data[0]]: `${year}-${month}-${day}`
+    };
+
+    console.log(sendObj);
+    (async function() {
+      const sending = await Backend.postData(sendObj);
+      console.log(sendObj, sending);
+    })();
+  };
+
   return (
     <>
       {hasEditableClass && (
@@ -54,22 +65,35 @@ export default function Text({ data, fullTr }) {
           contentEditable={true}
           suppressContentEditableWarning={true}
           onFocus={e => {
-            e.target.parentElement.classList.add('focus');
+            tdRef.current.classList.add('focus');
           }}
           onBlur={e => {
-            e.target.parentElement.classList.remove('focus');
+            tdRef.current.classList.remove('focus');
             hideSaveButton(e);
           }}
           onInput={e => {
             if (displaySaveButton) return;
             showSaveButton(e);
-            showDatePicker(e);
           }}
         >
           {currentTxt}
         </span>
       )}
-      {isDate && <DatePicker selected={publishDate} />}
+      {isDate && (
+        <DatePicker
+          selected={publishDate}
+          dateFormat="dd-MM-yyyy"
+          onChange={date => {
+            saveSelectedDate(date);
+          }}
+          onCalendarOpen={() => {
+            tdRef.current.classList.add('focus');
+          }}
+          onCalendarClose={() => {
+            tdRef.current.classList.remove('focus');
+          }}
+        />
+      )}
       {!hasEditableClass && !isDate && <span>{data[1]}</span>}
       <SaveButton
         displaySaveButton={displaySaveButton}
